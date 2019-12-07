5a677209d07126a02fa2f95d5193b83d3aaf6448
Subrat Behera <skb@outlook.in>
2019-12-07 21:45:34 +0530

    Initial starter project commit

---
+++ file-upload-download-rest-api/src/main/java/com/subrat/app/demo/FileUploadDownloadRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.subrat.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDownloadRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadDownloadRestApiApplication.class, args);
+	}
+
+}
